1ace0bc8fbdcd280ae4f4edc14ffd9a0f37f2eb1
Saamer Mansoor <i@saamer.me>
2021-11-29 22:29:55 -0500

    Blank SwiftUI TVOS template

---
+++ GettingStarted-tvOS/GettingStarted-tvOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GettingStarted-tvOS
+//
+//  Created by Saamer Mansoor on 11/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GettingStarted-tvOS/GettingStarted-tvOS/GettingStarted_tvOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStarted_tvOSApp.swift
+//  GettingStarted-tvOS
+//
+//  Created by Saamer Mansoor on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct GettingStarted_tvOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
